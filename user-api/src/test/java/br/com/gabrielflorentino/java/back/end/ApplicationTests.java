b3d2f90ed7eecc777bff35d37a35cdcb5c8f2068
Gabriel Florentino <gabrieleduflorentino@hotmail.com>
2026-07-24 16:20:14 -0300

    Configurações iniciais do projeto

---
+++ user-api/src/test/java/br/com/gabrielflorentino/java/back/end/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielflorentino.java.back.end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
