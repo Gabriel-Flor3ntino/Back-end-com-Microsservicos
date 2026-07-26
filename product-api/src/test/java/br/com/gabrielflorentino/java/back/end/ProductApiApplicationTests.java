267473bede812e5eaed171a3ff24084b926a5c2b
Gabriel Florentino <gabrieleduflorentino@hotmail.com>
2026-07-25 23:50:55 -0300

    Configurações do Docker

--- product-api/src/test/java/br/com/gabrielflorentino/java/back/end/ProductApiApplicationTests.java
+++ product-api/src/test/java/br/com/gabrielflorentino/java/back/end/ProductApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShoppingClientApplicationTests {
+class ProductApiApplicationTests {
 
 	@Test
 	void contextLoads() {
--- shopping-api/src/main/java/br/com/gabrielflorentino/java/back/end/service/ProductService.java
+++ shopping-api/src/main/java/br/com/gabrielflorentino/java/back/end/service/ProductService.java
@@ -1,24 +1,37 @@
 package br.com.gabrielflorentino.java.back.end.service;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import br.com.gabrielflorentino.java.back.end.dto.ProductDTO;
-import br.com.gabrielflorentino.java.back.end.exception.UserNotFoundException;
+import br.com.gabrielflorentino.java.back.end.exception.ProductNotFoundException;
+import reactor.core.publisher.Mono;
 
 @Service
 public class ProductService {
-
+	
+	@Value("${PRODUCT_API_URL:http://localhost:54880}")
+	private String productApiURL;
+	
 	public ProductDTO getProductByIdentifier(String productIdentifier) {
+
 		try {
-			RestTemplate restTemplate = new RestTemplate();
-			String url = "http://localhost:8081/product/" + productIdentifier;
-			ResponseEntity<ProductDTO> response = restTemplate.getForEntity(url, ProductDTO.class);
-			return response.getBody();
-		} catch (HttpClientErrorException.NotFound e){
-			throw new UserNotFoundException();
+			WebClient webClient = WebClient.builder()
+					.baseUrl(productApiURL)
+					.build();
+
+			Mono<ProductDTO> product = webClient.get()
+					.uri("/product/" + productIdentifier)
+					.retrieve()
+					.bodyToMono(ProductDTO.class);
+
+			return product.block();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ProductNotFoundException();
 		}
+
 	}
+
 }
